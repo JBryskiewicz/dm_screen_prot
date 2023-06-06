eeb6fc7be025a0f153851f385fd584c41a6632ea
JBryskiewicz <kuba@fiku.eu>
2023-06-06 13:43:48 +0200

    left navigation panel view with links to navigate around webapp

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import PrimarySearchAppBar from "./components/header/PrimarySearchAppBar";
+import LeftNavigation from "./components/leftMenu/LeftNavigation";
 
 function App() {
     return (
         <>
-            <PrimarySearchAppBar />
-            <h1>DM SCREEN v0.1</h1>
+            <PrimarySearchAppBar/>
+            {/*use MUI's grid tags*/}
+            <section style={{display: 'flex'}}>
+                <LeftNavigation/>
+                <h1>DM SCREEN v0.1</h1>
+            </section>
         </>
     );
 }
